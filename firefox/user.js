b5377341d9fdb98856d21d2d471f4566c0ab42a7
Chris Midgley <chris.midgley@dunecomputers.co.uk>
2021-06-01 23:19:01 +0100

    firefox: update for proton

--- firefox/user.js
+++ firefox/user.js
@@ -1,2 +1,5 @@
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-user_pref("browser.urlbar.trimURLs", false);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true); // allow userChrome.css
+user_pref("browser.compactmode.show", true); // allow compact mode
+
+user_pref("browser.urlbar.trimURLs", false); // show https:// and http://
+user_pref("ui.systemUsesDarkTheme", 0); // tell web pages to use light theme
