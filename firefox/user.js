5afc5f505ffb69e1efe98651a84538b3f386342f
Chris Midgley <chris.midgley@dunecomputers.co.uk>
2021-08-21 09:55:13 +0100

    firefox: disable accessibility for better perf

--- firefox/user.js
+++ firefox/user.js
@@ -3,3 +3,6 @@ user_pref("browser.compactmode.show", true); // allow compact mode
 
 user_pref("browser.urlbar.trimURLs", false); // show https:// and http://
 user_pref("ui.systemUsesDarkTheme", 0); // tell web pages to use light theme
+
+user_pref("accessibility.force_disabled", 1); // disable accessibility services, better perf
+// https://www.reddit.com/r/firefox/comments/p8g5zd/why_does_disabling_accessibility_services_improve/
