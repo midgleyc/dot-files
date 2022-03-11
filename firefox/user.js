bd3943738cc34fd240fe2eb742ac5d5f7aeedd10
Chris Midgley <chris.midgley@dunecomputers.co.uk>
2022-03-11 22:58:51 +0000

    firefox: disable download panel

--- firefox/user.js
+++ firefox/user.js
@@ -6,3 +6,4 @@ user_pref("ui.systemUsesDarkTheme", 0); // tell web pages to use light theme
 
 user_pref("accessibility.force_disabled", 1); // disable accessibility services, better perf
 // https://www.reddit.com/r/firefox/comments/p8g5zd/why_does_disabling_accessibility_services_improve/
+user_pref("browser.download.alwaysOpenPanel", false) // don't open download panel during download
